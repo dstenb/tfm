3b1b110993a95345dd7bd887c069b732f4c155c9
dstenb <dstenb@gmail.com>
2011-08-14 15:34:05 +0200

    cleaned up key handling in list state

--- src/list.c
+++ src/list.c
@@ -3,17 +3,64 @@
 static void activate();
 
 static void cmd_action(wdata_t *data);
+static void cmd_chcmd(wdata_t *data);
 static void cmd_chwin(wdata_t *data);
+static void cmd_end(wdata_t *data);
 static void cmd_go_down(wdata_t *data);
 static void cmd_go_up(wdata_t *data);
+static void cmd_home(wdata_t *data);
 static void cmd_previous_dir(wdata_t *data);
 static void cmd_toggle_dotfiles(wdata_t *data);
 static void cmd_toggle_sort(wdata_t *data);
+
+struct {
+	int key;
+	void (*func)(wdata_t *data);
+} list_cmds[] = {
+	{ 13, cmd_action },
+	{ 259, cmd_go_up },
+	{ 258, cmd_go_down },
+	{ 'k', cmd_go_up },
+	{ 'j', cmd_go_down },
+	{ 'g', cmd_home },
+	{ 'G', cmd_end },
+	{ 't', cmd_chwin },
+	{ 's', cmd_toggle_sort },
+	{ 'H', cmd_toggle_dotfiles },
+	{ 'u', cmd_previous_dir },
+	{ ':', cmd_chcmd }
+};
 
 void
 activate()
 {
 	set_message(M_INFO, "");
+}
+
+state *
+list_state()
+{
+	state *s;
+	
+	if (!(s = malloc(sizeof(state))))
+		oom();
+	s->keycmd = list_handle_key;
+	s->activate = activate;
+	s->normal_bindings = 1;
+	return s;
+}
+
+void
+list_handle_key(wdata_t *data, int c)
+{
+	int i;
+
+	for (i = 0; i < ARRSIZE(list_cmds); i++) {
+		if (c == list_cmds[i].key) {
+			list_cmds[i].func(data);
+			break;
+		}
+	}
 }
 
 void
@@ -31,9 +78,21 @@ cmd_action(wdata_t *data)
 }
 
 void
+cmd_chcmd(wdata_t *data)
+{
+	states_push(cmd_state());
+}
+
+void
 cmd_chwin(wdata_t *data)
 {
 	data->wsel = data->win[data->wsel == data->win[0]];
+}
+
+void
+cmd_end(wdata_t *data)
+{
+	dwindow_set_selected(data->wsel, data->wsel->size);
 }
 
 void
@@ -48,6 +107,12 @@ cmd_go_up(wdata_t *data)
 	if (data->wsel->sel.i > 0) {
 		dwindow_set_selected(data->wsel, data->wsel->sel.i - 1);
 	}
+}
+
+void
+cmd_home(wdata_t *data)
+{
+	dwindow_set_selected(data->wsel, 0);
 }
 
 void
@@ -87,45 +152,4 @@ cmd_toggle_sort(wdata_t *data)
 	dwindow_set_selected(dwin, 0);
 }
 
-state *
-list_state()
-{
-	state *s;
-	
-	if (!(s = malloc(sizeof(state))))
-		oom();
-	s->keycmd = list_handle_key;
-	s->activate = activate;
-	s->normal_bindings = 1;
-	return s;
-}
 
-void
-list_handle_key(wdata_t *data, int c)
-{
-	if (c == 't') {
-		cmd_chwin(data);
-	} else if (c == ':') {
-		states_push(cmd_state());
-/*	} else if (c == '/') {
-		states_push(search_state());
- */	} else if (c == 'u') {
-		cmd_previous_dir(data);
- 	} else if (c == 's') {
-		cmd_toggle_sort(data);
-	} else if (c == 'H') {
-		cmd_toggle_dotfiles(data);
-	} else if (has_selected_file(data->wsel)) {
-		if (c == 13) {
-			cmd_action(data);
-		} else if (c == 'j' || c == 258) {
-			cmd_go_down(data);
-		} else if (c == 'k' || c == 259) {
-			cmd_go_up(data);
-		} else if (c == 'g') {
-			dwindow_set_selected(data->wsel, 0);
-		} else if (c == 'G') {
-			dwindow_set_selected(data->wsel, data->wsel->size);
-		}
-	}
-}
